685fdfcf79002a3b3f3a2343e87e287f0667bd1c
KerinR432 <kerinaguilera@gmail.com>
2025-09-17 13:26:31 +0200

    Cosas nuevas de Python

---
+++ daw2-holamundo.py
@@ -0,0 +1,9 @@
+print("Hola mundo")
+# esto es un comentario
+
+"""
+Esto es un comentario de 
+bloque
+
+"""
+
---
+++ variables.py
@@ -0,0 +1,43 @@
+edad = 56  #variable numerica
+
+print(edad)
+
+texto = "Cincuenta seis" #variable de tipo texto
+print(texto)
+
+precio = 54.4 #variable decimal
+acertado = True #variable booleana
+
+print(precio)
+print(acertado)
+
+MESES_DEL_ANNO = 12 #no existen contaste solo es una indicación de que no teiene que cambiar este codigo
+
+#OPERADORES ARRITMETICOS
+print("--------------------")
+print(5//2) #el cociente
+print(5%2) #resto
+print(5/2) #division
+
+nuevo = precio * (5 + edad) #Prioridad de aritmetica
+print(nuevo)
+print(1/33) #decimales
+
+print("-------------------------")
+#CADENAS
+texto = "Hola"
+#texto = texto + " " + "Mundo"
+print(texto)
+print(texto + "Mundo")
+print(texto, "Mundo")
+curso = 2
+ciclo = "DAW"
+print(texto, "Mundo", end= " ",sep="-") # el sustituye por cualquier cosa el salto de linea SEP separa entre elemento
+print("Bienvenidos todos los de",curso,ciclo) #el print puede componer todo tipo de variable
+
+print("-------------------------")
+#RECORGER DATOS DEL TECLADO
+
+edad1 = input("Introduce tu edad: ") #es un input String
+print(edad1)
+
