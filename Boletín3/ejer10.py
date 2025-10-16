1d49fafd1c4bdd8c6722520e6e6557e042e3c8f3
Ruvik <Kerinaguilera@gmail.com>
2025-10-16 23:38:46 +0200

    Subir algo

--- Boletín3/ejer10.py
+++ Boletín3/ejer10.py
@@ -3,18 +3,7 @@ La longitud exacta de la cadena ha de ser de 9 caractéres✅. Los ocho primeros
 ser números comprendidos entre el 0 y el 9 y el último una letra que puede estar
 escrita en mayúsculas o minúsculas."""
 
-DNI = list(input("Intorduce tu DNI:\n"))
-DNITem = []
-print(DNI)
 
-if len(DNI)==9:
-    print("Bien tu DNI cumple con el tamaño😀")
-else:
-    print("Tu DNI no cumple con el tamaño❌")
-
-
-for i in range(len(DNI)-1):
-    DNITem.append(int(DNI[i]))
 
 
 for i in DNITem:
