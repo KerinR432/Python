e39f9850a1cf65cc199c41690fac061e82b997ed
KerinR432 <kerinaguilera@gmail.com>
2025-10-30 13:47:11 +0100

    conjuntos

---
+++ Conjuntos/Ejemplo.py
@@ -0,0 +1,26 @@
+conjunto1 = {"Ana","Pedro","Luis","Eva","Ana"}#* no lo almacena por orden, más bien van variando cada vez
+#*No varia. No permite tener dos mismas variables
+print(conjunto1)
+conjunto2 = set(["Ana","Pedro","Luis","Eva","Ana"]) #* Otra forma de llamar y combierte una lista en un conjunto
+print(conjunto2)
+
+for nombre in conjunto1:#* Podemos recorrer el conjunto asi
+    print(nombre)
+
+if "Ana" in conjunto1:  #* sí que se puede hacer una condición donde podremos ver si existe un nombre u otro elemento
+    print("Ana esta")
+
+print(len(conjunto1))
+
+conjunto1.add("Manuel")
+conjunto1.add("Manuel") #* Añadir elemento
+print(conjunto1)
+conjunto1.remove("Ana") #* Eliminar elementos
+print(conjunto1)
+conjunto1.discard("Eustaquio")#* si eliminamos algo que no existe, da error. Pero con 'discard' si puede eliminar
+
+print(conjunto1.pop()) #* Eliminamos el primero, nos lo recupera, pero de manera aletoria
+print(conjunto1)
+
+conjunto1.clear() #* Elimina todo el conjunto
+print(conjunto1)
